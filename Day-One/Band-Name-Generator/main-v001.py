fb59ee49555e0535e83d88606cba960b231e7fe4
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-11-12 08:41:17 +0600

    👶 version 0.0.1

---
+++ Day-One/Band-Name-Generator/main-v001.py
@@ -0,0 +1,5 @@
+print("welcome to Band Name Generator!\n")
+city = input('what is the name of the city you grow up!: ')
+pet = input('what is the name of your pet: ')
+
+print('\nYour Band Name Is: ' + city + ' ' + pet)
