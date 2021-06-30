a2c861712c69717ca426a4a8bfabab8f73a34f73
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-06-30 16:00:00 +0600

    oop turtle module demo

---
+++ 16-Day-Sixteen/Day-16-Start/turtle-module.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+jabba = Turtle()
+jabba.color('DeepPink')
+jabba.shape('turtle')
+jabba.forward(150)
+print(jabba)
+
+my_screen = Screen()
+my_screen.exitonclick()
