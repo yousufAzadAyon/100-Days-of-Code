c6ccfcbcd4cc8d7bc44b32b4fb7a1c74f8165e93
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-08-03 22:24:20 +0600

    modify turtle square

--- 18-Day-Eighteen/mail.py
+++ 18-Day-Eighteen/mail.py
@@ -2,13 +2,9 @@ from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
-timmy_the_turtle.fd(100)
-timmy_the_turtle.left(90)
-timmy_the_turtle.fd(100)
-timmy_the_turtle.left(90)
-timmy_the_turtle.fd(100)
-timmy_the_turtle.left(90)
-timmy_the_turtle.fd(100)
+for _ in range(4):
+    timmy_the_turtle.fd(100)
+    timmy_the_turtle.left(90)
 
 screen = Screen()
 screen.exitonclick()
