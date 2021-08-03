f798d45f9589c33accc7409d3a42c504a5d039f1
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-08-03 17:01:30 +0600

    day 18 making square with turtle

---
+++ 18-Day-Eighteen/mail.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.fd(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.fd(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.fd(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.fd(100)
+
+screen = Screen()
+screen.exitonclick()
