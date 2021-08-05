7e791bca2efb8369582d1b3a3c4697054038c9c0
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-08-05 09:37:55 +0600

    random walk test

--- 18-Day-Eighteen/mail.py
+++ 18-Day-Eighteen/mail.py
@@ -1,24 +1,24 @@
-from turtle import Turtle, Screen
-from random import choice
+from turtle import Turtle, Screen, colormode
+from random import choice, randint
 
 tim = Turtle()
+colormode(255)
 
-colors = ["CornflowerBlue","DarkOrchid","IndianRed","DeepSkyBlue","LightSeaGreen","weat","SlateGray"]
-shapes = ['arrow', 'turtle', 'circle', 'square', 'triangle', 'classic']
+def random_color():
+    r = randint(0,255)
+    g = randint(0,255)
+    b = randint(0,255)
+    color = (r,g,b)
+    return color
 
-def draw_shape(number_sides):
-    angle = 360 / number_sides
-    for _ in range(number_sides):
-        tim.fd(100)
-        tim.left(angle)
-    for _ in range(number_sides):
-        tim.fd(100)
-        tim.right(angle)
+directions = [0,90,180,270]
+tim.pensize(10)
+tim.speed("fast")
 
-for shape in range(3,11):
-    tim.color(choice(colors))
-    tim.shape(choice(shapes))
-    draw_shape(shape)
+for _ in range(500):
+    tim.color(random_color())
+    tim.fd(20)
+    tim.setheading(choice(directions))
 
 screen = Screen()
 screen.exitonclick()
