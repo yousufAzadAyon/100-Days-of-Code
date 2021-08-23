85b334070aebe814b8dda57e8308d599d5a51aa3
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-08-23 11:18:20 +0600

    refactor

--- 19-Day-Ninteen/etcher.py
+++ 19-Day-Ninteen/etcher.py
@@ -3,17 +3,22 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 
+
 def move_forward():
     tim.fd(10)
+
 
 def move_backwards():
     tim.backward(10)
 
+
 def turn_left():
     tim.left(10)
 
+
 def turn_right():
     tim.right(10)
+
 
 def clear():
     tim.clear()
@@ -21,10 +26,12 @@ def clear():
     tim.home()
     tim.pendown()
 
+
 screen.listen()
 screen.onkey(key="w", fun=move_forward)
 screen.onkey(key="s", fun=move_backwards)
 screen.onkey(key="a", fun=turn_left)
 screen.onkey(key="d", fun=turn_right)
 screen.onkey(key="c", fun=clear)
+
 screen.exitonclick()
