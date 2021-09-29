1c7fadfb74cf00251ee3bc8fa0a2d2f67d63cd6c
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-09-29 15:20:18 +0600

    turtle race

--- 19-Day-Ninteen/turtlerace.py
+++ 19-Day-Ninteen/turtlerace.py
@@ -1,7 +1,34 @@
 from turtle import Turtle, Screen
-
-tim = Turtle()
-jim = Turtle()
+import random
+is_race_on = False
 screen = Screen()
+screen.setup(width=500,height=400)
+user_bet = screen.textinput(title="User Bet", prompt="Enter a color you think will win")
+turtle_color = ["red", "green", "blue", "yellow", "purple", "brown"]
+y_position = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+
+for turtle_index in range(0,6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(turtle_color[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_position[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"congrats! {winning_color} has won!")
+            else:
+                print(f"Sorry! {winning_color} has won!")
+        random_distance = random.randint(0,9)
+        turtle.forward(random_distance)
+
 
 screen.exitonclick()
